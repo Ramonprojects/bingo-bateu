42e989c79212b4b9349965a68c38005528e76e79
Ramonprojects <adm@ramonmago.com>
2026-05-12 18:05:28 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://rbingob.magoramon.com.br/' });
+    res.end();                                                                                                                                                                             
+  }
