276c580127491addb8709a2f85a4deafc5828ca1
Ramonprojects <adm@ramonmago.com>
2026-05-13 11:18:10 -0300

    Update redirect URL in index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=SVIW86TWW0Bafp=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=SVIW86TWW0B&afp=bm&home=1' });
     res.end();
   }
