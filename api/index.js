248573df41837e1e430a7c06bc796f50e8482dda
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-15 13:53:04 -0300

    chore: update shareCode to MVLAX1BZRUP

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=ACC12U8R109&afp=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=MVLAX1BZRUP&afp=bm&home=1' });
     res.end();
   }
