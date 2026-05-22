4fa431da2b9e55f113fd8f74e10317faeea41e4e
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-22 09:52:05 -0300

    chore: update shareCode to GYN2MLMKQ8K

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=6YYM4WAESU5&afp=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=GYN2MLMKQ8K&afp=bm&home=1' });
     res.end();
   }
