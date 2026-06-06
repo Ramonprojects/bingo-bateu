341559adee82625988582b65ef4eba1a93597931
Ramonprojects <adm@ramonmago.com>
2026-06-06 09:06:17 -0300

    chore: shareCode P0U9TIF0CCV -> 6BVW9F27QEB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=P0U9TIF0CCV&afp3=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=6BVW9F27QEB&afp3=bm&home=1' });
     res.end();
   }
