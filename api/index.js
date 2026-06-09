9dadf9d5ef35a1e0163bf98cfd936341c3fa29f3
Ramonprojects <adm@ramonmago.com>
2026-06-09 15:54:55 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=5QOUKVAF0DY&afp2=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516?shareCode=CIUABKNAHTF&afp2=bm&home=1' });
     res.end();
   }
