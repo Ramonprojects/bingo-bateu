b54590307b6ec82663c42aa9cb98dfe40a056365
Ramonprojects <adm@ramonmago.com>
2026-06-11 11:26:43 -0300

    chore: shareCode PJCWIOB4KMC -> L1LWDFF8G9Q (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=PJCWIOB4KMC&afp2=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=L1LWDFF8G9Q&afp2=bm&home=1' });
     res.end();
   }
