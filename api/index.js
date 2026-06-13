9b2905ad66aa0f03d5bc8a2d0b7a773191331ae5
Ramonprojects <adm@ramonmago.com>
2026-06-13 10:19:33 -0300

    chore: shareCode 5XO4FVY1IPO -> IMFD95LZ85J (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=5XO4FVY1IPO&afp2=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=IMFD95LZ85J&afp2=bm&home=1' });
     res.end();
   }
