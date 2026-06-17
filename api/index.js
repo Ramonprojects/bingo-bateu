6df112e7c4d122ba9a7fbafa35a9abd21c767563
Ramonprojects <adm@ramonmago.com>
2026-06-17 10:50:45 -0300

    chore: shareCode 1JYXPXAZXUC -> 8SDM7S7KDBJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=1JYXPXAZXUC&afp2=bm&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=8SDM7S7KDBJ&afp2=bm&home=1' });
     res.end();
   }
